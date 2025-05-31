8e4ec85e44e2f65ca1b08fce5edd9ed93e5ed3eb
harishshete <42139616+harishshete@users.noreply.github.com>
2025-06-01 02:21:12 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 const status = require('./status');
-const contacts = require('../routes/contactsRoutes');
+const contacts = require('./contactsRoutes');
 
 
 app.use('/',status);
