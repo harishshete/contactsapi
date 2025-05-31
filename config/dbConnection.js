9f59617ed18aa1d4f6756e5af22ac39fb8f37f14
Harish Shete <harishshete430@gmail.com>
2025-06-01 02:37:06 +0530

    updated dbConnection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-console.log("printing URL variable => " + process.env.URL)
-console.log("printing LOCAL_URL variable => " + process.env.LOCAL_URL)
+//console.log("printing URL variable => " + process.env.URL)
+//console.log("printing LOCAL_URL variable => " + process.env.LOCAL_URL)
 
 mongoose.connect(`${process.env.URL }/${process.env.DB_NAME}`, {
     useNewURLParser: true,
