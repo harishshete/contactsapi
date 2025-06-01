08aaedab9b4728998058cd97b367472f7f2288b7
Harish Shete <harishshete430@gmail.com>
2025-06-01 10:31:30 +0530

    updated contactsController

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -4,7 +4,7 @@ const contacts = require('../controller/contactsController');
 
 router.post('/create/:pass_string',contacts.creaateContact);
 router.get('/getcontacts/:pass_string',contacts.getAllContacts);
-router.put('/update/:pass_string',contacts.updateContact)
+router.patch('/update/:pass_string',contacts.updateContact)
 router.delete('/delete/:pass_string',contacts.deleteContact)
 
 module.exports = router;
